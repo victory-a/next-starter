8afb1ac6e742bdc035bf70e85dea22d966ba4c9e
victory-a <victoryasokomeh@gmail.com>
2022-05-05 02:57:56 +0100

    chore: lint setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-    return <p>index page</p>;
+  return <p>index page</p>;
 };
 
 export default Home;
